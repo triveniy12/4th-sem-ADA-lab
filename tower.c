b31f23b8b2758b5d830e9f37ec82079ea69f9829
triveniy12 <55705306+triveniy12@users.noreply.github.com>
2020-02-05 12:48:50 +0530

    Add files via upload

---
+++ tower.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void tower(int,char,char,char);
+int main()
+{
+	int num;
+	printf("Enter the number of disks:");
+	scanf("%d",&num);
+	printf("The sequence of moves in the tower of hanoi are:\n");
+	towers(num,'A','C','B');
+	return 0;
+}
+void towers(int num,char frompeg,char topeg,char auxpeg)
+{
+ 	if(num==1)
+ 	{
+ 		printf("Move disk 1 from peg %c to peg %c",frompeg,topeg);
+ 		return;
+ 	}
+ 	towers(num-1,frompeg,auxpeg,topeg);
+ 	printf("Move disk %d from peg %c to %c",num,frompeg,topeg);
+ 	towers(num-1,auxpeg,topeg,frompeg);
+ }
